77080dd05a6a174f9b65c45117e0ee697315918e
pritish-phdata <psingha@phdata.io>
2024-01-24 20:09:04 +0530

    working

--- function_app.py
+++ function_app.py
@@ -16,12 +16,19 @@ async def http_start(req: func.HttpRequest, client):
 @myApp.orchestration_trigger(context_name="context")
 def hello_orchestrator(context):
     # test = print(context.get_input('config/params.json'))
-    with open('config/params.json') as f:
-        data = json.load(f)
+    path = 'config/params.json'
+    list_of_customers = yield context.call_activity("list_customers",path)
     result1 = yield context.call_activity("hello_city", "Bengaluru")
     result2 = yield context.call_activity("hello_state", "Karnataka")
     result3 = yield context.call_activity("hello_country", "India")
-    return [result1, result2, result3, data[0]["customer1"]]
+    return [result1, result2, result3, list_of_customers]
+
+# list the customers
+@myApp.activity_trigger(input_name="path")
+def list_customers(path: str):
+    with open(path) as f:
+        customer_list = json.load(f) 
+    return f"{customer_list}"
 
 # Activity function 1
 @myApp.activity_trigger(input_name="city")
